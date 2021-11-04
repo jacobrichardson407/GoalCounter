7b78f758425e54e6d725dc3544ef4f53f4062e83
Jacob Richardson <jacobrichardson407@gmail.com>
2021-11-03 21:13:15 -0400

    Initial Commit

---
+++ java.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("counterPlus1btn"),
+  count = 0;
+button.onclick = function() {
+  count += 1;
+  document.getElementById("counterPlus1").innerHTML = count;
+};
+var button = document.getElementById("counterPlus2btn"),
+  count1 = 0;
+button.onclick = function() {
+  count1 += 1;
+  document.getElementById("counterPlus2").innerHTML = count1;
+};
+var button = document.getElementById("counterPlus3btn"),
+  count2 = 0;
+button.onclick = function() {
+  count2 += 1;
+  document.getElementById("counterPlus3").innerHTML = count2;
+};
+var button = document.getElementById("counterMinus1btn"),
+  count3 = 0;
+button.onclick = function() {
+  count3 += 1;
+  document.getElementById("counterMinus1").innerHTML = count3;
+};
+var button = document.getElementById("counterMinus2btn"),
+  count4 = 0;
+button.onclick = function() {
+  count4 += 1;
+  document.getElementById("counterMinus2").innerHTML = count4;
+};
+var button = document.getElementById("counterMinus3btn"),
+  count5 = 0;
+button.onclick = function() {
+  count5 += 1;
+  document.getElementById("counterMinus3").innerHTML = count5;
+};
